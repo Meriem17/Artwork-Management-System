c622c7dfe84daed358f1cd20d3f6b8040be4ff92
MariemSoualhia <99603900+MariemSoualhia@users.noreply.github.com>
2023-01-25 19:38:48 +0100

    fichierouvre

--- backend/app/Models/FicheOeuvre.php
+++ backend/app/Models/FicheOeuvre.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\ouvrage;
 class FicheOeuvre extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+    protected $primaryKey = 'id';
+    protected $table = 'ficheOeuvres';
+    protected $fillable = [
+        'nomRedacteur',
+        'DateRedaction',
+        'dateModification',
+        'ouvrage_id',
+     ];
+     public function ouvrage()
+     {
+         return $this->belongsTo(ouvrage::class, 'foreign_key');
+     }
 }
